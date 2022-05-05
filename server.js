5d985f8e977d9c834a67c6853c74d8c0607d16a2
stanyzra <dantesparta100@gmail.com>
2022-05-05 15:48:26 -0300

    parece que foi em

--- server.js
+++ server.js
@@ -29,4 +29,5 @@ app.use("/api/presentations", presentationRoutes);
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`start on ${PORT} `));
+app.listen(PORT);
+console.log(`start on ${PORT} `)
